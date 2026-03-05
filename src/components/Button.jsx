4d1fbcc44e5ec081228e71d4bc219d1d2bc990e7
leeeunji <ayozz6742@naver.com>
2026-03-05 14:45:46 +0900

    Design: 버튼컴포넌트 코드 재수정

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,101 +1,105 @@
+// Button.jsx (Styled Components 사용 예시)
 import styled, { css } from 'styled-components';
+import ArrowIcon from '../assets/icons/icon-arrow-right.svg';
 
-const BUTTON_STYLE = {
-  primary: css`
-    background: var(--brown-40);
-    color: white;
-
-    &:hover {
-      background: var(--brown-40);
-      box-shadow: 0 0 0 2px var(--brown-50) inset;
-    }
-
-    &:active {
-      background: var(--brown-50);
-    }
-
-    &:disabled {
-      opacity: 0.5;
-      cursor: default;
-      pointer-events: none;
-    }
-    &::after {
-      filter: invert(1);
-    }
-  `,
-  outline: css`
-    background: var(--brown-10);
-    color: var(--brown-40);
-    box-shadow: 0 0 0 1px var(--brown-40) inset;
-    &:hover {
-      background: var(--brown-10);
-      box-shadow: 0 0 0 2px var(--brown-40) inset;
-    }
-
-    &:active {
-      background: var(--brown-20);
-    }
-    &:disabled {
-      opacity: 0.5;
-      background: var(--brown-10);
-      cursor: default;
-      pointer-events: none;
-    }
-  `,
-};
-
-const Button = styled.button`
+const StyledButton = styled.button`
   display: inline-flex;
   align-items: center;
   font-family: 'pretendard';
-  height: ${({ $small }) => ($small ? '34px' : '46px')};
-  padding: ${({ $small }) => ($small ? '0px 12px' : '0px 24px')};
-  font-size: ${({ $small }) => ($small ? '14px' : '16px')};
-  border-radius: ${({ $round }) => ($round ? '200px' : '8px')};
-  ${({ $arrow }) =>
-    $arrow &&
+  background: var(--brown-40) url(ArrowIcon);
+  color: white;
+  height: 46px;
+  font-size: 16px;
+  padding: 0px 24px;
+  border: none;
+  border-radius: 8px;
+  &:hover {
+    background: var(--brown-40);
+    box-shadow: 0 0 0 2px var(--brown-50) inset;
+  }
+
+  &:active {
+    background: var(--brown-50);
+  }
+
+  &:disabled {
+    opacity: 0.5;
+    cursor: default;
+    pointer-events: none;
+  }
+  img {
+    filter: invert(1);
+    width: 18px;
+    margin-left: 8px;
+  }
+  // 1. 크기 (Size) 설정
+  ${({ size }) =>
+    size === 'small' &&
     css`
-      &::after {
-        content: '';
-        display: inline-block;
-        vertical-align: middle;
-        width: 18px;
-        height: 18px;
-        margin-left: 8px;
-        background-image: url('src/assets/icons/icon-arrow-right.svg');
-        background-size: cover;
-      }
+      padding: 0px 12px;
+      font-size: 14px;
+      height: 34px;
     `}
 
-  ${({ type }) => BUTTON_STYLE[type]}
+  // 2. 종류 (Variant) 설정
+  ${({ variant }) =>
+    variant === 'outline' &&
+    css`
+      background-color: var(--brown-10);
+      color: var(--brown-40);
+      &:hover {
+        background: var(--brown-10);
+        box-shadow: 0 0 0 2px var(--brown-40) inset;
+      }
+      &:active {
+        background: var(--brown-20);
+      }
+      &:disabled {
+        opacity: 0.5;
+        background: var(--brown-10);
+      }
+      img {
+        filter: invert(0);
+      }
+    `}
 `;
 
-export default Button;
-
-// 버튼 적용법
-{
-  /* <Button type="primary" $arrow disabled>
-        질문 받기
-      </Button>
-      <Button type="primary" $arrow>
-        질문 받기
-      </Button>
-      <Button type="outline" $arrow disabled>
-        답변하러 가기
-      </Button>
-      <Button type="outline" $arrow>
-        답변하러 가기
-      </Button>
-      <Button type="primary" $arrow $small>
-        질문 받기
-      </Button>
-      <Button type="outline" $arrow $small>
-        답변하러 가기
-      </Button>
-      <Button type="primary" $arrow $small disabled>
-        질문 받기
-      </Button>
-      <Button type="outline" $arrow $small >
-        답변하러 가기
-      </Button> */
+export default function Button({
+  variant = 'primary',
+  size = 'medium',
+  children,
+  ...props
+}) {
+  return (
+    <StyledButton variant={variant} size={size} {...props}>
+      {children}
+      <img src={ArrowIcon} />
+    </StyledButton>
+  );
 }
+
+/* 사용 예시
+
+  <Button variant="primary">기본 버튼</Button>
+  <Button variant="outline">작은 버튼</Button>
+  <Button variant="primary" size="small">
+    기본 버튼
+  </Button>
+  <Button variant="outline" size="small">
+    작은 버튼
+  </Button>
+
+  // Inactivate 상태
+  <Button variant="primary" disabled>
+    기본 버튼
+  </Button>
+  <Button variant="outline" disabled>
+    작은 버튼
+  </Button>
+  <Button variant="primary" size="small" disabled>
+    기본 버튼
+  </Button>
+  <Button variant="outline" size="small" disabled>
+    작은 버튼
+  </Button>
+*/
